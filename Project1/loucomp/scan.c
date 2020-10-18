c115b5cdc3605df82da223714b2093fe83283a5b
이정재 <wjd8637@naver.com>
2020-10-19 00:21:23 +0900

    JJ

--- Project1/loucomp/scan.c
+++ Project1/loucomp/scan.c
@@ -268,6 +268,7 @@ TokenType getToken(void)
    }
    if (TraceScan) {
      fprintf(listing,"\t%d: ",lineno);
+     fprintf(listing,"\t%d: ",lineno);
      printToken(currentToken,tokenString);
    }
    return currentToken;
